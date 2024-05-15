22e5e536dc6c10339ef622b314ad1dcb10d3b9c1
Angela Tolwani <angela@tolwani.com>
2024-05-15 14:02:39 -0400

    parent child control example

--- phase-2/control/code/beginning/src/App.js
+++ phase-2/control/code/beginning/src/App.js
@@ -14,14 +14,26 @@ Core Deliverables
 
 // To make `Card` a child of `App`, we need to import it (and render it) inside `App`.
 import Card from "./Card";
+import { useState } from "react"; // any function starting with "use" isn't given by default in React
+
 
 export default function App() {
+  const [cardValue, setCardValue] = useState("Ace of Spades");
+  const [borderColor, setBorderColor] = useState("black");
+  function randomize () {
+    const suit = ["Spades", "Clubs", "Hearts", "Diamonds"];
+    const cardNumber = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"];
+    const randomSuit = suit[Math.floor(Math.random() * suit.length)];
+    const randomCardNumber = cardNumber[Math.floor(Math.random() * cardNumber.length)];
+    setCardValue(`${randomCardNumber} of ${randomSuit}`);
+    randomSuit==="Hearts" || randomSuit==="Diamonds" ? setBorderColor("red") : setBorderColor("black");
+  };
   return (
     <div style={{ minHeight: "100vh",
                   display: "flex", flexDirection: "column",
                   alignItems: "center", justifyContent: "center" }}>
       <h1>Is This Your Card?</h1>
-      <Card />
+      <Card currentCard = {cardValue} changeFunction = {randomize} borderColor={borderColor}/>
       <footer>&copy; 2023 Sakib Rasul</footer>
     </div>
   );
--- phase-2/control/code/beginning/src/Card.js
+++ phase-2/control/code/beginning/src/Card.js
@@ -1,11 +1,11 @@
-export default function Card() {
+export default function Card({currentCard, changeFunction, borderColor}) {
     return (
         <h2 style={{ padding: "1rem",
                      display: "flex", flexDirection: "column", gap: "12px", alignItems: "center",
-                     border: "solid black", borderRadius: "10px"
+                     border: `solid ${borderColor}`, borderRadius: "10px"
         }}>
-            Ace of Spades
-            <button>Nah.</button>
+            {currentCard}
+            <button onClick={changeFunction}>Nah.</button>
         </h2>
     )
   }
