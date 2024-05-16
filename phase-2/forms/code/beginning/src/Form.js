c9a4fad10362e2dfe1440099f4da1315460fac04
Angela Tolwani <angela@tolwani.com>
2024-05-16 14:44:23 -0400

    complete lab

--- phase-2/forms/code/beginning/src/Form.js
+++ phase-2/forms/code/beginning/src/Form.js
@@ -1,15 +1,32 @@
+import { useState } from "react";
+
 export default function Form() {
+    // const [ name, setName ] = useState("");
+    // const [ movie, setMovie] = useState("");
+    const [ memberData, setMemberData ] = useState({
+        name: "",
+        movie: ""
+    });
+    function handleChange(event) {
+        if (event.target.value[0] === event.target.value[0].toUpperCase()) {
+            setMemberData({
+                ...memberData, 
+                [event.target.name]: event.target.value})
+            } else {
+                alert("First letter must be a capital letter!")
+            }
+    };
     return (
         <form>
             <label><strong>Name </strong>
-                <input />
+                <input name="name" value={memberData.name} onChange={handleChange}/>
             </label>
             <br/><br/>
             <label><strong>Movie </strong>
-                <input />
+                <input name="movie" value={memberData.movie} onChange={handleChange}/>
             </label>
             <br/><br/>
-            <em>Your lucky number is</em> <strong>2</strong>!
+            <em>Your lucky number is</em> <strong>{memberData.name.length+memberData.movie.length}</strong>!
             <br/><br/>
             <input type="submit" value="Sign Up"/>
             <h2>Members</h2>
